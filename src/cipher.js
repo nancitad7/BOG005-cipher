7824483000d2b0734c4ea412ad8119eb5e8d370c
nancitad7 <108703214+nancitad7@users.noreply.github.com>
2022-08-08 12:49:40 -0500

    sube todo el proyecto

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,57 @@
 const cipher = {
-  // ...
-};
+
+  encode: function (desplazamiento, mensaje1) {
+    let resultado = "";
+    if (desplazamiento === null || mensaje1 === null|| typeof desplazamiento!== "number"||typeof mensaje1!== "string"){
+
+      throw new TypeError("ingresa tu mensaje");
+    }
+
+    for (let i = 0; i < mensaje1.length; i++) {
+
+      if (desplazamiento && mensaje1) {
+        let codigoAcci = mensaje1.charCodeAt(i);
+        if (codigoAcci > 64 && codigoAcci < 91) {
+
+          let mensajeCifrado = (((codigoAcci - 65 + parseInt(desplazamiento) % 26) % 26) + 65);
+          resultado = resultado + String.fromCharCode(mensajeCifrado);
+        }
+        else {
+          resultado = resultado + String.fromCharCode(codigoAcci);
+        }
+      }
+    }
+    return resultado
+  },
+  decode: function (desplazamiento, mensaje1) {
+    //mensaje1 -> ABC, desplazarse -> 1
+
+    let resultado = "";
+    if (desplazamiento === null || mensaje1 === null|| typeof desplazamiento!== "number"||typeof mensaje1!== "string"){
+
+      throw new TypeError("ingresa tu mensaje");
+    }
+
+    for (let i = 0; i < mensaje1.length; i++) {
+      // ABC
+      // 012 --> por aca navega la i
+      if (desplazamiento && mensaje1) {
+        let codigoAcci = mensaje1.charCodeAt(i);
+        // codigoAcci -> 65 cuando i es 0 y asi sucesivamente
+        if (codigoAcci > 33 && codigoAcci < 91) {
+
+          let caracterDescifrado = (((codigoAcci + 65 - (parseInt(desplazamiento) % 26)) % 26) + 65);
+          resultado = resultado + String.fromCharCode(caracterDescifrado);
+        }
+        else {
+          resultado = resultado + String.fromCharCode(codigoAcci);
+        }
+
+      }
+    }
+    return resultado
+  }
+}
 
 export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,80 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const btnmostrar = document.getElementById("mostrar");
+btnmostrar.addEventListener("click", mostrarCajas)
+function mostrarCajas() {
+
+  document.getElementById("caja_fondos").style.display = "block";
+  document.getElementById("caja_principal").style.display = "none";
+}
+
+const btnocultar = document.getElementById("ocultar");
+btnocultar.addEventListener("click", ocultarCajas)
+function ocultarCajas() {
+
+  document.getElementById("caja_fondos").style.display = "none";
+  document.getElementById("caja_principal").style.display = "block";
+}
+
+function mayusculas() {
+  // tomamos el valor del input
+  var texto = document.getElementById("mensaje1").value;
+  // lo modificamos
+  var mayu = texto.toUpperCase();
+  // le reasignamos el valor modificado
+  document.getElementById("mensaje1").value = mayu;
+}
+const texMay = document.getElementById("mensaje1");
+texMay.addEventListener("keyup", mayusculas);
+
+
+function cifrando() {
+
+  let texto = document.getElementById("mensaje1").value;
+  let desplazamiento = parseInt(document.getElementById("desplazarse").value);
+  if (texto == "") {
+    alert("introduce un mensaje")
+  }
+  if (document.getElementById("desplazarse").value == "") {
+    alert("introduce numero de dezplazamiento")
+  }
+
+  let textoCifrado = cipher.encode(desplazamiento, texto);
+  document.getElementById("mensaje2").value = textoCifrado;
+
+}
+
+const boton = document.getElementById("cifrar");
+
+boton.addEventListener("click", cifrando);
+
+function descifrando() {
+  let texto = document.getElementById("mensaje1").value;
+  let desplazamiento = parseInt(document.getElementById("desplazarse").value);
+  document.getElementById("mensaje2");
+
+  let textoDescifrado = cipher.decode(desplazamiento, texto);
+  document.getElementById("mensaje2").value = textoDescifrado;
+}
+
+const botn = document.getElementById("descifrar");
+
+botn.addEventListener("click", descifrando);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
